18cae18f74c1cb1ae549e640ad0155ea18424431
Ali Bas <alibas01@gmail.com>
2021-08-22 15:36:37 -0400

    navbar restyling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,24 +9,29 @@ import "../style/Nav.css";
 export default function Nav(props) {
   const { show, setShow, difficulty, setDifficulty } = props;
 
+  //const handleClick = (e) => {
+
+
   return (
     <nav className="nav">
-      <div className="dropdown" onClick={() => setShow((s) => !s)} >
-        <button className="dropbtn">Difficulty:{difficulty} <i className="fas fa-caret-down"></i></button>
+      <div className="dropdown">
+        <button className="dropbtn" onClick={() => setShow((s) => !s)} >Difficulty<i className="fas fa-caret-down"></i></button>
         <div className="dropdown-content" style={{ display: show ? "block" : "none" }}>
-          <p onClick={setDifficulty("easy")}>Easy</p>
-          <p onClick={setDifficulty("medium")}>Medium</p>
-          <p onClick={setDifficulty("hard")}>Hard</p>
+          <p onClick={() => setDifficulty("easy")}>Easy</p>
+          <p onClick={() => setDifficulty("medium")}>Medium</p>
+          <p onClick={() => setDifficulty("hard")}>Hard</p>
         </div>
       </div>
+      <div className="difficulty">
+        <button className="diffbtn" onClick={() => setShow((s) => !s)} >{difficulty} </button>
+      </div>
+      <div className="clock">
+        <i className="far fa-clock"></i>
+        &nbsp;&nbsp;
+        <Timer />
+      </div>
         <ul className="nav-menu">
-          <li className="nav-menu-item">
-            <i className="far fa-clock"></i>
-            &nbsp;&nbsp;
-            <div>
-            <Timer />
-            </div>
-          </li>
+
           <li className="nav-menu-item">
           <i class="fas fa-flag-checkered fa-lg"></i>
             &nbsp;&nbsp;
